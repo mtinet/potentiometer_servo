f8ecb4d20b913ac52a23464ead121395d8ebb2c8
Jason Juhyun Kim <mtinet@hanmail.net>
2017-02-15 01:19:35 +0900

    Create potentiometer_servo.ino

---
+++ potentiometer_servo.ino
@@ -0,0 +1,23 @@
+// potentiometer_servo
+
+#include <Servo.h>
+int sensorPin = A0;
+int val = 0;
+Servo servo1;
+
+void setup()
+{
+  servo1.attach(9);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  val = analogRead(sensorPin);
+  Serial.print(val);
+  Serial.print("   ");
+  val = map(val, 0, 1024, 0,180);
+  servo1.write(val);
+  Serial.println(val);
+  delay(100);
+}
